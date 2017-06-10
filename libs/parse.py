3012f10f603ab2d7d265a0d55e61168e3b51b0b3
katsuya0719 <katsuya0719@gmail.com>
2017-06-10 11:35:55 +0900

    changed index page

--- libs/parse.py
+++ libs/parse.py
@@ -1,1 +1,18 @@
+import requests
+from lxml import html
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import os
+
+class base():
+    def __init__(self,url):
+        self.get_page(url)
+
+    def get_page(self,url):
+        html = urlopen(url)
+        self.bsObj = BeautifulSoup(html, "html.parser")
+
+if __name__ == '__main__':
+    #os.environ.setdefault("DJANGO_SETTINGS_MODULE", "lovadeck.settings")
+    url=
 
