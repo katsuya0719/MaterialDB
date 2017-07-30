e4cd15d07874a4f4371c710cbd9ec73a644a3045
katsuya0719 <katsuya0719@gmail.com>
2017-07-30 18:18:12 +0900

    will push to github

--- Envelope/admin.py
+++ Envelope/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
-from .models import Glass
+from .models import Glass,Insulation
 #from import_export import resources
 #from import_export.admin import ImportExportModelAdmin
 
 # Register your models here.
 admin.site.register(Glass)
+admin.site.register(Insulation)
 
 """
 class GlassResource(resources.ModelResource):
